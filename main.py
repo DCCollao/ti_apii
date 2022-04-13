7241e7880b40575e6ee97b8385cb0b0ea8fdb498
Diego Collao <diego.collao@vestua.com>
2022-04-13 17:30:11 -0400

    fix page

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from httplib2 import Response
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('pagina.html')
